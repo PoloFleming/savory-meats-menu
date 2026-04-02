cc4bb3908a32f6b3c6734f2b77820001469725ed
PoloFleming <108616352+PoloFleming@users.noreply.github.com>
2026-04-02 18:25:47 +0200

    Update GrillSection.tsx

--- src/components/menu/GrillSection.tsx
+++ src/components/menu/GrillSection.tsx
@@ -12,88 +12,88 @@ export const GrillSection = () => {
     <MenuCategory title={t("grillMeat")}>
       <MenuItem
         title={t("spiedoMisto")}
-        price="23.00"
+        price="24.00"
         description={t("spiedoMistoDesc")}
       />
       <MenuItem
         title={t("spiedoDiSalsicciaMista")}
-        price="23.00"
+        price="24.00"
         description={t("spiedoDiSalsicciaMistaDesc")}
       />
       <MenuItem
         title={t("spiedoDiBombette")}
-        price="23.00"
+        price="24.00"
         description={t("spiedoDiBombetteDesc")}
       />
       <MenuItem
         title={t("spiedoDiSalsicciaAPuntaDiColtello")}
-        price="23.00"
+        price="24.00"
         description={t("spiedoDiSalsicciaAPuntaDiColtelloDesc")}
       />
       <MenuItem
         title={t("spiedoDiBombetteDiCinghiale")}
-        price="23.00"
+        price="24.00"
         description={t("spiedoDiBombetteDiCinghialeDesc")}
       />
       <MenuItem
         title={t("spiedoDiSalsicciaPiccante")}
-        price="23.00"
+        price="24.00"
         description={t("spiedoDiSalsicciaPiccanteDesc")}
       />
       <MenuItem
         title={t("spiedoDiTurcinieddi")}
-        price="23.00"
+        price="24.00"
         description={t("spiedoDiTurcinieddiDesc")}
       />
       <MenuItem
         title={t("spiedoDiPolpaDiCastrato")}
-        price="23.00"
+        price="24.00"
         description={t("spiedoDiPolpaDiCastratoDesc")}
       />
       <MenuItem
         title={t("fiorentinaGrill")}
-        price="6.00"
+        price="6.50"
         description={t("fiorentinaGrillDesc")}
         priceDisclosure="/100g"
       />
       <MenuItem
         title={t("costataDiVitello")}
-        price="6.00"
+        price="6.50"
         description={t("costataDiVitelloDesc")}
         priceDisclosure="/100g"
       />
       <MenuItem
         title={t("tagliataDiManzo")}
-        price="6.00"
+        price="6.50"
         description={t("tagliataDiManzoDesc")}
         priceDisclosure="/100g"
       />
       <MenuItem
         title={t("tagliataDiLattoneGrill")}
-        price="6.00"
+        price="6.50"
         description={t("tagliataDiLattoneGrillDesc")}
         priceDisclosure="/100g"
       />
       <MenuItem
         title={t("disossataDiScottoneGrill")}
-        price="6.00"
+        price="6.50"
         description={t("disossataDiScottoneGrillDesc")}
         priceDisclosure="/100g"
       />
       <MenuItem
         title={t("capicolloDiCinghiale")}
-        price="6.00"
+        price="6.50"
         description={t("capicolloDiCinghialeDesc")}
         priceDisclosure="/100g"
       />
       <MenuItem
         title={t("agnello")}
-        price="40.00"
+        price="45.00"
         description={t("agnelloDesc")}
       />
       <MenuItem
         title={t("castrato")}
-        price="6.00"
+        price="6.50"
         description={t("castratoDesc")}
         priceDisclosure="/100g"
       />
